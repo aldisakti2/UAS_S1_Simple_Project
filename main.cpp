0c00f32349c3f31413ed9c11a36503ef721c887d
aldisakti2 <aldialvayadi2@gmail.com>
2022-01-11 14:06:10 +0700

    Tampilan Menu v1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+void menu(){
+    cout << "================================\n";
+    cout << "======== MENU MODUL C++ ========\n";
+    cout << "================================\n" << endl;
+    cout << "1. Input dan Output\n";
+    cout << "2. Percabangan\n";
+    cout << "3. Perulangan\n";
+    cout << "4. Array\n";
+    cout << "5. Sorting\n";
+    cout << "6. Searching\n";
+    cout << "7. Rekursif\n\n";
+}
+
 int main()
 {
-    cout << "Testing" << endl;
+    int pilih;
+    menu();
+    cout << "Masukkan Pilihan Anda (1-7): "; cin >> pilih;
     return 0;
 }
